18e3c34dc22d80ceb3d0ef1f0e7e87b65f738e29
ihciah <ihciah@gmail.com>
2023-06-09 08:14:08 +0000

    add ParamRef and ParamMut

--- src/param.rs
+++ src/param.rs
@@ -2,8 +2,28 @@ pub trait Param<T> {
     fn param(&self) -> T;
 }
 
+pub trait ParamRef<T> {
+    fn param_ref(&self) -> &T;
+}
+
+pub trait ParamMut<T> {
+    fn param_mut(&mut self) -> &mut T;
+}
+
 impl<T: Clone> Param<T> for T {
     fn param(&self) -> T {
         self.clone()
+    }
+}
+
+impl<T> ParamRef<T> for T {
+    fn param_ref(&self) -> &T {
+        self
+    }
+}
+
+impl<T> ParamMut<T> for T {
+    fn param_mut(&mut self) -> &mut T {
+        self
     }
 }
