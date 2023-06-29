7bc8187ed684470b90d2859682daf462bf609e07
ihciah <ihciah@gmail.com>
2023-06-29 11:36:22 +0000

    chore: remove auto impl to allow user impl for T

--- param/src/lib.rs
+++ param/src/lib.rs
@@ -37,15 +37,3 @@ impl<T: Clone> Param<T> for T {
         self.clone()
     }
 }
-
-impl<T> ParamRef<T> for T {
-    fn param_ref(&self) -> &T {
-        self
-    }
-}
-
-impl<T> ParamMut<T> for T {
-    fn param_mut(&mut self) -> &mut T {
-        self
-    }
-}
